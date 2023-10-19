8a2a04a4c573a5e2e12d7306f78e00910d7a3a52
sung-yong-k <82442247+sung-yong-k@users.noreply.github.com>
2023-10-20 07:00:11 +0900

    Update starcoder_big.py

--- inference/starcoder_big.py
+++ inference/starcoder_big.py
@@ -140,7 +140,7 @@ checkpoint = "bigcode/starcoder"
 device = "cuda" # for GPU usage or "cpu" for CPU usage
 
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModelForCausalLM.from_pretrained(checkpoint,device_map="auto" , max_memory=get_gpus_max_memory("35GB")).to(device)
+model = AutoModelForCausalLM.from_pretrained(checkpoint,device_map="auto" , max_memory=get_gpus_max_memory("35GB"))
 
 
 
